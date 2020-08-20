4364069955ce294e463ecf9a98b22e14a322d3a3
nikitarasevskij <squidrash777@gmail.com>
2020-08-20 11:54:41 +0300

    часть из Filter переместил в GetFiles

--- Student/GetFiles.cs
+++ Student/GetFiles.cs
@@ -25,9 +25,28 @@ namespace Student
                 throw new Exception("Несуществует");
             }
         }
-        
+        public List<StudentDTO> Files2()
+        {
+            if (Directory.Exists(dirName))
+            {
+                var files = Directory.GetFiles(dirName)
+                    .Where(x => x.EndsWith(".json"))
+                    .Select(x => x.Replace("StudentCards/", ""));
+                foreach (var file in files)
+                {
+                    //Console.WriteLine(f);
+                    var path = Path.Combine("StudentCards", $"{file}");
 
-        
+                    var jsonString = File.ReadAllText(path);
+                    var student = JsonSerializer.Deserialize<StudentDTO>(jsonString);
+                    studentF.Add(student);
+                }
+                return studentF;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
     }
-
 }
